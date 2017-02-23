ce9ab377e7f567c90a47511964309ddefd756e4d
MrCooper42 <mr.cooper42co@gmail.com>
2017-02-23 13:26:27 -0700

    small d3 addition

---
+++ client/src/app/dice-charts/chartsState.ts
@@ -0,0 +1,3 @@
+// export class DieState {
+//     rollStats: [];
+// }
--- client/src/app/dice-charts/dice-charts.component.ts
+++ client/src/app/dice-charts/dice-charts.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit } from '@angular/core';
+import * as D3 from 'd3/index';
+
+//
 
 @Component({
   selector: 'app-dice-charts',
@@ -6,8 +9,68 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dice-charts.component.css']
 })
 export class DiceChartsComponent implements OnInit {
+@Input()
+// chartState: any;
+height;
+host;
+htmlElement: HTMLElement;
+margin;
+svg;
+width;
+xAxis;
+xScale;
+yAxis;
+yScale;
+zScale;
 
-  constructor() { }
+
+  constructor(private _element: ElementRef) {
+    this.host = D3.select(this._element.nativeElement)
+   }
+
+  ngOnChanges() {
+    this.setup();
+    this.buildSVG();
+    this.populate();
+    this.drawXAxis();
+    this.drawYAxis();
+  }
+
+  setup() {
+    this.margin = {
+      top: 15,
+      right: 50,
+      bottom: 40,
+      left: 50
+    };
+    this.width = document.querySelector('#chartplot').clientWidth - this.margin.left - this.margin.right;
+    this.height = this.width * 0.6 - this.margin.bottom - this.margin.top;
+    this.xScale = D3.scaleLinear().range([0, this.width]);
+    this.yScale = D3.scaleLinear().range([this.height, 0]);
+    this.zScale = D3.scaleLinear().range([2, 15]);
+  }
+
+  buildSVG() {
+  this.host.html('');
+  this.svg = this.host.append('svg')
+    .attr('width', this.width + this.margin.left + this.margin.right)
+    .attr('height', this.height + this.margin.top + this.margin.bottom)
+    .append('g')
+    .attr('transform', `translate(${this.margin.left}, ${this.margin.top})`);
+  }
+
+  drawXAxis() {
+    this.xAxis = D3.axisBottom(this.xScale)
+      .ticks(5)
+      .tickPadding(15);
+    this.svg.append('g')
+      .attr('class', 'label')
+      .attr('x', this.width)
+      .attr('y', -6)
+      .style('text-anchor', 'end')
+      .style('fill', 'grey')
+      .text('Roll Chance')
+}
 
   ngOnInit() {
   }
